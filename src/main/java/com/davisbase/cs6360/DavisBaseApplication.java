9626ef51d4aec169ae5693456a6bc5611f5f11f2
baansalravneet <34987414+baansalravneet@users.noreply.github.com>
2023-11-19 11:58:36 -0600

    initialised spring

---
+++ src/main/java/com/davisbase/cs6360/DavisBaseApplication.java
@@ -0,0 +1,13 @@
+package com.davisbase.cs6360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavisBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavisBaseApplication.class, args);
+	}
+
+}
