2b86d2120a9beffd4fd2b8b6380ff4200c21d68a
Aishwarya Kondaveeti <akondaveeti@Aishwaryas-Air.lan>
2023-12-03 11:57:01 -0600

    changes to the help command and queryparser

--- src/main/java/com/davisbase/commands/impl/HelpCommand.java
+++ src/main/java/com/davisbase/commands/impl/HelpCommand.java
@@ -4,6 +4,16 @@ import com.davisbase.commands.Command;
 import com.davisbase.commands.CommandContext;
 import com.davisbase.commands.output.CommandOutput;
 import com.davisbase.commands.output.impl.HelpOutput;
+import com.davisbase.commands.Command;
+import com.davisbase.commands.CommandContext;
+import com.davisbase.commands.output.CommandOutput;
+import com.davisbase.commands.output.impl.HelpOutput;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 public class HelpCommand extends Command {
 
@@ -16,10 +26,38 @@ public class HelpCommand extends Command {
         return printHelp();
     }
 
-    // TODO: Complete this. Ideally, maintain a txt file somewhere
-    // in the repo and dump that file on screen.
     public CommandOutput printHelp() {
-        return new HelpOutput("WIP");
+        try {
+            // Get the current working directory
+            String currentDir = System.getProperty("user.dir");
+            // Build the relative path to the help file
+            Path helpFilePath = Paths.get(currentDir,  "src", "main", "java", "com", "davisbase", "help.txt");
+
+            // Read the content of the help file
+            String helpText = Files.lines(helpFilePath)
+                    .collect(Collectors.joining("\n"));
+            return new HelpOutput(helpText);
+        } catch (IOException e) {
+            System.err.println("Error reading help file: " + e.getMessage());
+            return new HelpOutput("Help file not found.");
+        }
     }
-    
 }
+//public class HelpCommand extends Command {
+//
+//    public HelpCommand(CommandContext context) {
+//        super(context);
+//    }
+//
+//    @Override
+//    public CommandOutput execute() {
+//        return printHelp();
+//    }
+//
+//    // TODO: Complete this. Ideally, maintain a txt file somewhere
+//    // in the repo and dump that file on screen.
+//    public CommandOutput printHelp() {
+//        return new HelpOutput("WIP");
+//    }
+//
+//}
--- src/main/java/com/davisbase/services/components/impl/QueryParser.java
+++ src/main/java/com/davisbase/services/components/impl/QueryParser.java
@@ -6,8 +6,13 @@ import com.davisbase.commands.CommandType;
 import com.davisbase.commands.impl.CreateTableCommand;
 import com.davisbase.commands.impl.ExitCommand;
 import com.davisbase.commands.impl.HelpCommand;
+import com.davisbase.models.ColumnDefinition;
+import com.davisbase.models.DataType;
 import com.davisbase.services.Mediator;
 import com.davisbase.services.components.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class QueryParser extends Component {
 
@@ -31,6 +36,7 @@ public class QueryParser extends Component {
 		case HELP:
 			return new HelpCommand(null);
 		case CREATE_TABLE:
+
 			return generateCreateTableCommand(input);
 		case SHOW_TABLE:
 			return null;
@@ -53,15 +59,74 @@ public class QueryParser extends Component {
 
 	}
 
-	private CommandContext getCommandContext() {
-		CommandContext context = null;
+//	private CommandContext getCommandContext() {
+//		CommandContext context = null;
+//
+//		return context;
+//	}
+private CommandContext getCreateTableCommandContext(String input) {
+	CommandContext context = new CommandContext();
+	List<ColumnDefinition> columnDefinitions = new ArrayList<>();
 
-		return context;
+	System.out.println(input);
+
+	// Extract the part of the string inside the parentheses
+	int startIndex = input.indexOf("(") + 1;
+	int endIndex = input.lastIndexOf(")");
+	if (startIndex < 0 || endIndex < 0 || endIndex <= startIndex) {
+		// Handle error - invalid input format
+		return null;
+	}
+	String columnsPart = input.substring(startIndex, endIndex).trim();
+
+	// Split the extracted part into individual column definitions
+	String[] cols = columnsPart.split(",\\s*");
+
+	for (String col : cols) {
+		// Split each column definition into its parts
+		String[] parts = col.split("\\s+");
+		String columnName = parts[0];
+		DataType dataType = DataType.valueOf(parts[1].toUpperCase());
+
+		boolean isNotNull = col.contains("NO");
+		boolean isPrimaryKey = col.contains("PRI");
+		boolean isUnique = col.contains("UNI") && !isPrimaryKey;
+
+		ColumnDefinition colDef = new ColumnDefinition(columnName, dataType, isNotNull, isUnique, isPrimaryKey);
+		columnDefinitions.add(colDef);
+	}
+
+	// Set the column definitions in the context
+	context.setColumnContext(columnDefinitions);
+
+	// Extract and set the table name
+	String tableName = extractTableName(input);
+	System.out.println(tableName);
+	context.setTableName(tableName);
+
+	return context;
+}
+
+	private String extractTableName(String input) {
+		String[] parts = input.split("\\s+");
+		for (int i = 0; i < parts.length; i++) {
+			if (parts[i].equalsIgnoreCase("TABLE") && (i + 1) < parts.length) {
+				// Extracting table name and removing anything after '('
+				String tableName = parts[i + 1].trim();
+				int parenIndex = tableName.indexOf("(");
+				if (parenIndex != -1) {
+					tableName = tableName.substring(0, parenIndex);
+				}
+				return tableName;
+			}
+		}
+		return null; // Table name not found in the input
 	}
 
 	// TODO: complete this
 	private Command generateCreateTableCommand(String input) {
-		return new CreateTableCommand(null);
+
+		return new CreateTableCommand(getCreateTableCommandContext(input));
 	}
 
 }
